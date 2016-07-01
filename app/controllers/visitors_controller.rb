fcec0d8aec971b2d4a8d98b982482505a9dcdaf2
Saloneer <saloneers@catprint.com>
2016-07-01 12:23:32 -0400

    update front end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash[:notice] = 'Hello there!'
-    flash[:alert] = 'My birthday is coming up soon!'
   end
 
 end
