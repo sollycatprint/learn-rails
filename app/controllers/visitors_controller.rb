c05fc88dfb59a2ab71ff32709e6c0fafe0f9ee01
Saloneer <saloneers@catprint.com>
2016-07-01 11:49:08 -0400

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash[:notice] = 'Hello there!'
+    flash[:alert] = 'My birthday is coming up soon!'
   end
 
 end
