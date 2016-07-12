0cedb92860726c92a11ac01b44cb5213c4a31676
Saloneer <saloneers@catprint.com>
2016-07-12 11:11:50 -0400

    update contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,24 @@
 class ContactsController < ApplicationController
 
-  def process_form
-    if params[:contact][:name].blank?
-      raise 'Name is blank!'
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(secure_params)
+    if @contact.valid?
+      # TODO send message
+      flash[:notice] = "Message sent from #{@contact.name}."
+      redirect_to root_path
+    else
+      render :new
     end
-    if params[:contact][:email].blank?
-      raise 'Email is blank!'
-    end
-    if params[:contact][:content].blank?
-      raise 'Message is blank!'
-    end
-    message = "Received request from #{params[:contact][:name]}"
-    redirect_to root_path, :notice => message
+  end
+
+  private
+
+  def secure_params
+    params.require(:contact).permit(:name, :email, :content)
   end
 
 end
